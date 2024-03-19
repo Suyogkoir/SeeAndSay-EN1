d71a0af3d3fd42d8137bd27c2cd9010472edec40
suyog <suyogkoirala6@gmail.com>
2024-03-19 13:16:16 -0400

    Changes made

--- AnimalImpl/SampleAnimal.java
+++
@@ -1,5 +0,0 @@
-package SeeAndSay.AnimalImpl;
-
-//public class SampleAnimal extends Animal implements Talker {
-//
-//}
--- AnimalImpl/lu1127103.java
+++ AnimalImpl/lu1127103.java
@@ -11,7 +11,7 @@ public class lu1127103 extends Animal implements Talker {
 
     @Override
     public String getAnimalName() {
-        return "Witty Beaver";
+        return "Efficient Seal";
     }
 
     @Override
